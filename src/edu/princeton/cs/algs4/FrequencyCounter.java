bc036ab8e78e7b8cd553a31aa9887e514b765938
sam <sam@XT1-20141109HVS>
2015-03-31 18:05:39 +0800

    Computer elapsed time

--- src/edu/princeton/cs/algs4/FrequencyCounter.java
+++ src/edu/princeton/cs/algs4/FrequencyCounter.java
@@ -1,5 +1,7 @@
 package edu.princeton.cs.algs4;
-import  edu.princeton.cs.introcs.*;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
 
 /*************************************************************************
  *  Compilation:  javac FrequencyCounter.java
@@ -28,81 +30,81 @@ import  edu.princeton.cs.introcs.*;
  *************************************************************************/
 
 /**
- *  The <tt>FrequencyCounter</tt> class provides a client for 
- *  reading in a sequence of words and printing a word (exceeding
- *  a given length) that occurs most frequently. It is useful as
- *  a test client for various symbol table implementations.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The <tt>FrequencyCounter</tt> class provides a client for reading in a
+ * sequence of words and printing a word (exceeding a given length) that occurs
+ * most frequently. It is useful as a test client for various symbol table
+ * implementations.
+ * <p>
+ * For additional documentation, see <a
+ * href="http://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class FrequencyCounter {
 
-    /**
-     * Reads in a command-line integer and sequence of words from
-     * standard input and prints out a word (whose length exceeds
-     * the threshold) that occurs most frequently to standard output.
-     * It also prints out the number of words whose length exceeds
-     * the threshold and the number of distinct such words.
-     */
-    public static void main(String[] args) {
-        int distinct = 0, words = 0;
-        int minlen = Integer.parseInt(args[0]);
-        ST<String, Integer> st = new ST<String, Integer>();
+	/**
+	 * Reads in a command-line integer and sequence of words from standard input
+	 * and prints out a word (whose length exceeds the threshold) that occurs
+	 * most frequently to standard output. It also prints out the number of
+	 * words whose length exceeds the threshold and the number of distinct such
+	 * words.
+	 */
+	public static void main(String[] args) {
+		int distinct = 0, words = 0;
+		int minlen = Integer.parseInt(args[0]);
+		ST<String, Integer> st = new ST<String, Integer>();
+		Stopwatch timer = new Stopwatch();
+		// compute frequency counts
+		while (!StdIn.isEmpty()) {
+			String key = StdIn.readString();
+			if (key.length() < minlen)
+				continue;
+			words++;
+			if (st.contains(key)) {
+				st.put(key, st.get(key) + 1);
+			} else {
+				st.put(key, 1);
+				distinct++;
+			}
+		}
 
-        // compute frequency counts
-        while (!StdIn.isEmpty()) {
-            String key = StdIn.readString();
-            if (key.length() < minlen) continue;
-            words++;
-            if (st.contains(key)) {
-                st.put(key, st.get(key) + 1);
-            }
-            else {
-                st.put(key, 1);
-                distinct++;
-            }
-        }
-
-        // find a key with the highest frequency count
-        String max = "";
-        st.put(max, 0);
-        for (String word : st.keys()) {
-            if (st.get(word) > st.get(max))
-                max = word;
-        }
-
-        StdOut.println(max + " " + st.get(max));
-        StdOut.println("distinct = " + distinct);
-        StdOut.println("words    = " + words);
-    }
+		// find a key with the highest frequency count
+		String max = "";
+		st.put(max, 0);
+		for (String word : st.keys()) {
+			if (st.get(word) > st.get(max))
+				max = word;
+		}
+		double time = timer.elapsedTime();
+		StdOut.println(max + " " + st.get(max));
+		StdOut.println("distinct   = " + distinct);
+		StdOut.println("words      = " + words);
+		StdOut.println("elapsedtime= " + time + "s");
+	}
 }
 
-
 /*************************************************************************
- *  Copyright 2002-2012, Robert Sedgewick and Kevin Wayne.
+ * Copyright 2002-2012, Robert Sedgewick and Kevin Wayne.
  *
- *  This file is part of algs4-package.jar, which accompanies the textbook
+ * This file is part of algs4-package.jar, which accompanies the textbook
  *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
  *
  *
- *  algs4-package.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * algs4-package.jar is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- *  algs4-package.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License
- *  along with algs4-package.jar.  If not, see http://www.gnu.org/licenses.
+ * algs4-package.jar is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * algs4-package.jar. If not, see http://www.gnu.org/licenses.
  *************************************************************************/
 
