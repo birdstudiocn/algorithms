a9f572464f356b233107e8895923e9e7290fd689
sam <bird_studio@163.com>
2015-05-25 10:18:34 +0800

    add distTo property

--- src/edu/princeton/cs/algs4/DepthFirstPaths.java
+++ src/edu/princeton/cs/algs4/DepthFirstPaths.java
@@ -1,5 +1,7 @@
 package edu.princeton.cs.algs4;
-import  edu.princeton.cs.introcs.*;
+
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdOut;
 
 /*************************************************************************
  *  Compilation:  javac DepthFirstPaths.java
@@ -30,124 +32,140 @@ import  edu.princeton.cs.introcs.*;
  *************************************************************************/
 
 /**
- *  The <tt>DepthFirstPaths</tt> class represents a data type for finding
- *  paths from a source vertex <em>s</em> to every other vertex
- *  in an undirected graph.
- *  <p>
- *  This implementation uses depth-first search.
- *  The constructor takes time proportional to <em>V</em> + <em>E</em>,
- *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
- *  It uses extra space (not including the graph) proportional to <em>V</em>.
- *  <p>
- *  For additional documentation, see <a href="/algs4/41graph">Section 4.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The <tt>DepthFirstPaths</tt> class represents a data type for finding paths
+ * from a source vertex <em>s</em> to every other vertex in an undirected graph.
+ * <p>
+ * This implementation uses depth-first search. The constructor takes time
+ * proportional to <em>V</em> + <em>E</em>, where <em>V</em> is the number of
+ * vertices and <em>E</em> is the number of edges. It uses extra space (not
+ * including the graph) proportional to <em>V</em>.
+ * <p>
+ * For additional documentation, see <a href="/algs4/41graph">Section 4.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class DepthFirstPaths {
-    private boolean[] marked;    // marked[v] = is there an s-v path?
-    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
-    private final int s;         // source vertex
+	private boolean[] marked; // marked[v] = is there an s-v path?
+	private int[] edgeTo; // edgeTo[v] = last edge on s-v path
+	private int[] distTo; // distTo[v] = length of shortest s->v path
+	private final int s; // source vertex
 
-    /**
-     * Computes a path between <tt>s</tt> and every other vertex in graph <tt>G</tt>.
-     * @param G the graph
-     * @param s the source vertex
-     */
-    public DepthFirstPaths(Graph G, int s) {
-        this.s = s;
-        edgeTo = new int[G.V()];
-        marked = new boolean[G.V()];
-        dfs(G, s);
-    }
+	/**
+	 * Computes a path between <tt>s</tt> and every other vertex in graph
+	 * <tt>G</tt>.
+	 * 
+	 * @param G
+	 *            the graph
+	 * @param s
+	 *            the source vertex
+	 */
+	public DepthFirstPaths(Graph G, int s) {
+		this.s = s;
+		edgeTo = new int[G.V()];
+		distTo = new int[G.V()];
+		marked = new boolean[G.V()];
+		dfs(G, s);
+	}
 
-    // depth first search from v
-    private void dfs(Graph G, int v) {
-        marked[v] = true;
-        for (int w : G.adj(v)) {
-            if (!marked[w]) {
-                edgeTo[w] = v;
-                dfs(G, w);
-            }
-        }
-    }
+	// depth first search from v
+	private void dfs(Graph G, int v) {
+		marked[v] = true;
+		for (int w : G.adj(v)) {
+			if (!marked[w]) {
+				edgeTo[w] = v;
+				distTo[w] = distTo[v] + 1;
+				dfs(G, w);
+			}
+		}
+	}
 
-    /**
-     * Is there a path between the source vertex <tt>s</tt> and vertex <tt>v</tt>?
-     * @param v the vertex
-     * @return <tt>true</tt> if there is a path, <tt>false</tt> otherwise
-     */
-    public boolean hasPathTo(int v) {
-        return marked[v];
-    }
+	/**
+	 * Is there a path between the source vertex <tt>s</tt> and vertex
+	 * <tt>v</tt>?
+	 * 
+	 * @param v
+	 *            the vertex
+	 * @return <tt>true</tt> if there is a path, <tt>false</tt> otherwise
+	 */
+	public boolean hasPathTo(int v) {
+		return marked[v];
+	}
 
-    /**
-     * Returns a path between the source vertex <tt>s</tt> and vertex <tt>v</tt>, or
-     * <tt>null</tt> if no such path.
-     * @param v the vertex
-     * @return the sequence of vertices on a path between the source vertex
-     *   <tt>s</tt> and vertex <tt>v</tt>, as an Iterable
-     */
-    public Iterable<Integer> pathTo(int v) {
-        if (!hasPathTo(v)) return null;
-        Stack<Integer> path = new Stack<Integer>();
-        for (int x = v; x != s; x = edgeTo[x])
-            path.push(x);
-        path.push(s);
-        return path;
-    }
+	/**
+	 * Returns a path between the source vertex <tt>s</tt> and vertex <tt>v</tt>
+	 * , or <tt>null</tt> if no such path.
+	 * 
+	 * @param v
+	 *            the vertex
+	 * @return the sequence of vertices on a path between the source vertex
+	 *         <tt>s</tt> and vertex <tt>v</tt>, as an Iterable
+	 */
+	public Iterable<Integer> pathTo(int v) {
+		if (!hasPathTo(v))
+			return null;
+		Stack<Integer> path = new Stack<Integer>();
+		for (int x = v; x != s; x = edgeTo[x])
+			path.push(x);
+		path.push(s);
+		return path;
+	}
 
-    /**
-     * Unit tests the <tt>DepthFirstPaths</tt> data type.
-     */
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Graph G = new Graph(in);
-        int s = Integer.parseInt(args[1]);
-        DepthFirstPaths dfs = new DepthFirstPaths(G, s);
+	public int distTo(int v) {
+		return distTo[v];
+	}
 
-        for (int v = 0; v < G.V(); v++) {
-            if (dfs.hasPathTo(v)) {
-                StdOut.printf("%d to %d:  ", s, v);
-                for (int x : dfs.pathTo(v)) {
-                    if (x == s) StdOut.print(x);
-                    else        StdOut.print("-" + x);
-                }
-                StdOut.println();
-            }
+	/**
+	 * Unit tests the <tt>DepthFirstPaths</tt> data type.
+	 */
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		Graph G = new Graph(in);
+		int s = Integer.parseInt(args[1]);
+		DepthFirstPaths dfs = new DepthFirstPaths(G, s);
 
-            else {
-                StdOut.printf("%d to %d:  not connected\n", s, v);
-            }
+		for (int v = 0; v < G.V(); v++) {
+			if (dfs.hasPathTo(v)) {
+				StdOut.printf("%d to %d (%d):  ", s, v, dfs.distTo(v));
+				for (int x : dfs.pathTo(v)) {
+					if (x == s)
+						StdOut.print(x);
+					else
+						StdOut.print("-" + x);
+				}
+				StdOut.println();
+			}
 
-        }
-    }
+			else {
+				StdOut.printf("%d to %d:  not connected\n", s, v);
+			}
+
+		}
+	}
 
 }
 
-
 /*************************************************************************
- *  Copyright 2002-2012, Robert Sedgewick and Kevin Wayne.
+ * Copyright 2002-2012, Robert Sedgewick and Kevin Wayne.
  *
- *  This file is part of algs4-package.jar, which accompanies the textbook
+ * This file is part of algs4-package.jar, which accompanies the textbook
  *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
  *
  *
- *  algs4-package.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * algs4-package.jar is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- *  algs4-package.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License
- *  along with algs4-package.jar.  If not, see http://www.gnu.org/licenses.
+ * algs4-package.jar is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * algs4-package.jar. If not, see http://www.gnu.org/licenses.
  *************************************************************************/
 
